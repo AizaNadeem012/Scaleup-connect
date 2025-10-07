bb948481aaa140fc6e18bdef57622e8b0955700c
Aiza Nadeem <164611470+AizaNadeem012@users.noreply.github.com>
2025-10-07 11:29:08 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/hero-section"
-import AboutSection from "@/components/about-section"
+// import AboutSection from "@/components/about-section"
 // import Mask from "@/components/mask"
 import ServicesSection from "@/components/services-section"
 import PartnersSection from "@/components/partners-section"
@@ -10,7 +10,7 @@ import FounderSection from "@/components/founder"
 import Testinomial1 from "@/components/testinomial1"
 import Carosel from "@/components/carosel"
 import { Footer } from "@/components/footer"
-import MarqueeSection from "@/components/marque"
+
 import Navigation from "@/components/navigation"
 import StatsSection from "@/components/stats-section"
 import ProcessSection from "@/components/process-section"
@@ -29,8 +29,7 @@ export default function HomePage() {
     <main className="min-h-screen w-full overflow-x-hidden">
       <Navigation />
       <HeroSection />
-      {/* <Mask/> */}
-      <MarqueeSection />
+    
       <ServicesSection />
       <FounderSection />
       <MarqueeBrand/>
